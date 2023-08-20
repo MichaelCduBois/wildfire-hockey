b265c3d4f7cc972934d3f78897337f01e0b4cbd8
Michael duBois <mdubois@bytement.com>
2023-08-20 13:37:16 -0600

    Add Svelte Static Wildfire Site with Deployments

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+  <img src="Wildfire Text.png" alt="Wildfire Text" class="w-1/2 mx-auto" />
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from "@sveltejs/adapter-static";
+import preprocess from 'svelte-preprocess';
+
+const dev = process.argv.includes('dev');
+
+export default {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
+  }
+};
