368284201cca7b85bf86cd4177cc93b55e4a50aa
Michael duBois <mdubois@bytement.com>
2023-08-20 17:33:26 -0600

    Add Formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes("dev");
 
 export default {
   preprocess: preprocess(),
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
+      pages: "build",
+      assets: "build",
       fallback: undefined,
       precompress: false,
-      strict: true
+      strict: true,
     }),
     paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-    }
-  }
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+  },
 };
