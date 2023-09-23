a08a0af478806b99d63ecceaa58a16eea9f15c6a
Michael duBois <mdubois@bytement.com>
2023-09-22 23:55:56 -0600

    Update Test Formatting

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h2", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("heading", { name: "A big thank you to all of our sponsors!" })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "A big thank you to all of our sponsors!" })
+  ).toBeVisible();
 });
