0148049fe397802cad0e78f85762f45c698b322a
Michael duBois <mdubois@bytement.com>
2023-10-18 05:49:25 -0600

    Add Tailwind Dynamic Class Workaround

--- src/lib/components/sponsors/sponsorrow.svelte
+++ src/lib/components/sponsors/sponsorrow.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   export let sponsorCount: number;
 
-  function getClasses(sponsorCount: number) {
-    return `logo-cloud grid-cols-1 lg:!grid-cols-${sponsorCount} gap-1`;
-  }
+  // Tailwind workaround for dynamic classes
+  const _possibleClasses: Array<string> = [
+    "lg:!grid-cols-1",
+    "lg:!grid-cols-2",
+    "lg:!grid-cols-3",
+    "lg:!grid-cols-4",
+    "lg:!grid-cols-5",
+  ]
+  // Use the _possibleClasses for linting
+  _possibleClasses;
 </script>
 
-<div class={getClasses(sponsorCount)}>
+<div class="logo-cloud grid-cols-1 lg:!grid-cols-{sponsorCount} gap-1">
   <slot />
 </div>
